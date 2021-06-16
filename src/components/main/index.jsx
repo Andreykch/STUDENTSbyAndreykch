81b48423e3da72eb133ea26bb35e1bc0184f80fe
Andreykch <Andr9879@yandex.ru>
2021-06-16 14:03:30 +0500

    fix main component

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,11 +1,11 @@
-import React from "react"
-import SerachAndSort from "../serachAndSortWrapper/index.jsx"
-import Table from "../table/index.jsx"
-import TextHeader from "../textHeader/index.jsx"
-import { MainContainer } from "./styled.jsx"
+import React from 'react'
+import SerachAndSort from '../serachAndSortWrapper/index.jsx'
+import Table from '../table/index.jsx'
+import TextHeader from '../textHeader/index.jsx'
+import { MainContainer } from './styled.jsx'
 
-function Main() {
-    return <MainContainer>
+function Main () {
+  return <MainContainer>
         <TextHeader header="Студенты"/>
         <SerachAndSort/>
         <Table/>
